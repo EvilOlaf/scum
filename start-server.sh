56f84a387a5cff70b762c0733f5488fbc3b428c5
Werner <3305006+EvilOlaf@users.noreply.github.com>
2025-11-17 19:12:39 +0100

    Fix typos in start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-
 export WINEDEBUG=-all
 export WINEARCH=win64
 export WINEPREFIX=/opt/wine64
@@ -9,11 +8,11 @@ export XDG_RUNTIME_DIR=/tmp
 
 # skip wineboot if initialized already
 if [ ! -d "$WINEPREFIX" ]; then
-  echo "Init wine..."
+  echo "Initialize wine..."
   wineboot --init && sleep 5
 fi
 
-echo "Installung/updating SCUM dedicated server..."
+echo "Installing/updating SCUM dedicated server..."
 /opt/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows \
   +force_install_dir /opt/scumserver \
   +login anonymous \

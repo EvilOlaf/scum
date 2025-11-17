e4a6dc9f6a629350ec256c583d68b77c320180aa
Werner <3305006+EvilOlaf@users.noreply.github.com>
2025-11-17 17:27:09 +0100

    Refactor wine setup and server launch commands
    Updated wine initialization and server start command.

--- start-server.sh
+++ start-server.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Init wine..."
-
-if ! command -v wine64 >/dev/null 2>&1; then
-  echo "Wine not found, installing..."
-  dpkg --add-architecture i386
-  apt-get update
-  apt-get install -y wine wine64 wine32 libwine libwine:i386 winbind
-fi
 
 export WINEDEBUG=-all
 export WINEARCH=win64
@@ -16,19 +8,20 @@ export WINEPREFIX=/opt/wine64
 
 # skip wineboot if initialized already
 if [ ! -d "$WINEPREFIX" ]; then
+  echo "Init wine..."
   wineboot --init && sleep 5
 fi
 
 echo "Installung/updating SCUM dedicated server..."
 /opt/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows \
-  +login anonymous \
   +force_install_dir /opt/scumserver \
+  +login anonymous \
   +app_update 3792580 validate \
   +quit
 
 echo "Starting SCUM dedicated server..."
 xvfb-run --auto-servernum --server-args="-screen 0 1024x768x24" \
-  wine64 /opt/scumserver/SCUM/Binaries/Win64/SCUMServer.exe \
+  wine /opt/scumserver/SCUM/Binaries/Win64/SCUMServer.exe \
     -log \
     -port=7777 \
     -QueryPort=27015 \

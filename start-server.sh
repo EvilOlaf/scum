c4a54270e449fae843ef87a7561daca1aab492f1
Werner <3305006+EvilOlaf@users.noreply.github.com>
2025-11-21 05:41:33 +0100

    remove duplicates
    - environment variables are set in Dockerfile already
    - same story for wineboot

--- start-server.sh
+++ start-server.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-export WINEDEBUG=-all
-export WINEARCH=win64
-export WINEPREFIX=/opt/wine64
-export XDG_RUNTIME_DIR=/tmp
-
-# skip wineboot if initialized already
-if [ ! -d "$WINEPREFIX" ]; then
-  echo "Initialize wine..."
-  wineboot --init && sleep 5
-fi
-
 echo "Installing/updating SCUM dedicated server..."
 /opt/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows \
   +force_install_dir /opt/scumserver \

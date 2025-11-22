615cc5f3091071f9a9dede2d78cc7d9e489047fa
Werner <3305006+EvilOlaf@users.noreply.github.com>
2025-11-22 07:44:40 +0100

    add variable ports to startup

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Installing/updating SCUM dedicated server..."
+echo "Installing/updating steamcmd and SCUM dedicated server..."
 /opt/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows \
   +force_install_dir /opt/scumserver \
   +login anonymous \
@@ -12,6 +12,6 @@ echo "Starting SCUM dedicated server..."
 xvfb-run --auto-servernum --server-args="-screen 0 1024x768x24" \
   wine /opt/scumserver/SCUM/Binaries/Win64/SCUMServer.exe \
     -log \
-    -port=7777 \
-    -QueryPort=27015 \
+    -port=${PORT-7777} \
+    -QueryPort=${QUERYPORT-27015} \
     -MaxPlayers=32

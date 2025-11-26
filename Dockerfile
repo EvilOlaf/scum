0a81f783b294131772e8fa9088f3cf2d2294ad63
Werner <3305006+EvilOlaf@users.noreply.github.com>
2025-11-26 09:01:10 +0100

    move steamcmd from image to runtime (#7)
    * move steamcmd from image to runtime
    
    * fix typo
    
    * add wget failsafe switches

--- Dockerfile
+++ Dockerfile
@@ -22,11 +22,9 @@ ENV WINEDEBUG -all
 ENV WINEARCH win64
 ENV WINEPREFIX /opt/wine64
 ENV XDG_RUNTIME_DIR /tmp
+ENV PATH=/opt/steamcmd:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 RUN wineboot --init
-
-RUN mkdir -p /opt/steamcmd && cd /opt/steamcmd && wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz && tar -xvzf steamcmd_linux.tar.gz
-ENV PATH=/opt/steamcmd:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # scum server run script
 COPY start-server.sh /opt/start-server.sh
@@ -34,7 +32,7 @@ RUN chmod +x /opt/start-server.sh
 
 # cleanup
 RUN apt-get clean
-RUN rm /opt/steamcmd/steamcmd_linux.tar.gz
+RUN rm -rf /var/lib/apt/lists/*
 
 EXPOSE 27020/udp
 EXPOSE 27015/udp
--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "Installing/updating steamcmd and SCUM dedicated server..."
+# skip download of SteamCMD if present already
+if [ ! -f /opt/steamcmd/steamcmd.sh ]; then
+    echo "SteamCMD not found. Installing..."
+    mkdir -p /opt/steamcmd && \
+    cd /opt/steamcmd && \
+    wget --timeout=30 --tries=3 https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz && \
+    tar -xvzf steamcmd_linux.tar.gz && \
+    rm /opt/steamcmd/steamcmd_linux.tar.gz && \
+    echo "SteamCMD successfully installed" 
+else
+    echo "SteamCMD found, skipping installation..."
+fi
+
+echo "Update SteamCMD and SCUM dedicated server..."
 /opt/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows \
   +force_install_dir /opt/scumserver \
   +login anonymous \
